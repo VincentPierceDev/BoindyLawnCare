6c1a35e3a020f50274736e2aab528a72d7145fd9
Vincent <vincentpierce.dev@gmail.com>
2025-08-27 02:15:39 -0500

    about section almost finished

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -12,56 +12,6 @@ export default defineConfig({
   build: {
       inlineStylesheets: 'never',
   },
-
-  experimental: {
-    fonts: [
-        {
-            provider: "local",
-            name: "Raleway",
-            cssVariable: "--heading-font",
-            variants: [
-                {
-                    src: ["./src/assets/fonts/raleway/custom-400.woff2"],
-                    weight: 400,
-                    style: "normal"
-                },
-                {
-                    src: ["./src/assets/fonts/raleway/custom-700.woff2"],
-                    weight: 700,
-                    style: "normal"
-                },
-                {
-                    src: ["./src/assets/fonts/raleway/custom-900.woff2"],
-                    weight: 900,
-                    style: "normal"
-                }
-            ]
-        },
-        {
-            provider: "local",
-            name: "Roboto",
-            cssVariable: "--body-font",
-            variants: [
-               {
-                    src: ["./src/assets/fonts/roboto/custom-400.woff2"],
-                    weight: 400,
-                    style: "normal"
-                },
-                {
-                    src: ["./src/assets/fonts/roboto/custom-700.woff2"],
-                    weight: 700,
-                    style: "normal"
-                },
-                {
-                    src: ["./src/assets/fonts/roboto/custom-900.woff2"],
-                    weight: 900,
-                    style: "normal"
-                } 
-            ]
-        }
-    ]
-  },
- 
   trailingSlash: 'never',
   base: '/',
   output: 'static',
